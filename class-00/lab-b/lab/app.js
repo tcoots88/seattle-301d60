a2b0b1847e36e27e1070b739bb92161471087055
tcoots88 <travis.coots@yahoo.com>
2019-12-02 22:25:36 -0800

    completed all steps

--- class-00/lab-b/lab/app.js
+++ class-00/lab-b/lab/app.js
@@ -156,26 +156,26 @@ const message = name => `Hello, ${name}!`;
 console.log(message('Allie'));
 
 
-// let Student = function(name, age, hometown) {
-//   this.name = name;
-//   this.age = age;
-//   this.hometown = hometown;
-// };
+let Student = function(name, age, hometown) {
+  this.name = name;
+  this.age = age;
+  this.hometown = hometown;
+};
 
 
 let joe = new Student('Joe', 'Schmoe', 100);
 
-const Student = array => ({
-  name: array[0],
-  age: array[1],
-  hometown: array[2]
-});
+// const Student = array => ({
+//   name: array[0],
+//   age: array[1],
+//   hometown: array[2]
+// });
 
-console.log(Student(['Joe', 'Schmoe', 100]));
+// console.log(Student(['Joe', 'Schmoe', 100]));
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
@@ -185,6 +185,9 @@ Student.prototype.greeting = function() {
   return `Hi, my name is ${this.name}`;
 };
 
+// const greeting = course => `Hi, my name is ${name}`;
+
+
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
 console.log(joe.greeting());
@@ -193,12 +196,14 @@ console.log(joe.greeting());
 
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+// Student.courseName = function() {
+//   return 'This student is enrolled in Code 301.';
+// };
+
+Student.courseName = () => 'This student is enrolled in Code 301.';
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -209,17 +214,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// Object { name: "Joe", age: "Schmoe", hometown: 100 }, the definition of the 'joe' variable
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// I got Window http://127.0.0.1:5500/....which is my port for this liveserver....
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// It is different because of the scope involved when using an arrow function.
